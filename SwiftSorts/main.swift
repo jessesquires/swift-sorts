6fec120a4485db0e3bc4be203f3c438cb55e03de
Jesse Squires <jesse.d.squires@gmail.com>
2014-06-03 22:16:31 -0700

    Initial Commit

---
+++ SwiftSorts/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSorts
+//
+//  Created by Jesse Squires on 6/3/14.
+//  Copyright (c) 2014 Hexed Bits. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
