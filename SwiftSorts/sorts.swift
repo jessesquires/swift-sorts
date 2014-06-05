2d0a59c86a932babd1be2e2276d094e9d74f79fa
Jesse Squires <jesse.d.squires@gmail.com>
2014-06-05 01:44:15 -0700

    add quick sort

--- SwiftSorts/sorts.swift
+++ SwiftSorts/sorts.swift
@@ -49,3 +49,43 @@ func insertionSort(var arr: Int[])
         arr[j] = target
     }
 }
+
+func partition(arr: Int[], left: Int, right: Int) -> Int
+{
+    var i = left
+    var j = right
+    var pivot = arr[(left + right) / 2]
+    
+    while i <= j {
+        while arr[i] < pivot {
+            i++
+        }
+        
+        while j > 0 && arr[j] > pivot {
+            j--
+        }
+        
+        if i <= j {
+            swap(&arr[i], &arr[j])
+            i++
+            
+            if j > 0 {
+                j--
+            }
+        }
+    }
+    return i
+}
+
+func quickSort(arr: Int[], left: Int, right: Int)
+{
+    var index = partition(arr, left, right)
+    
+    if left < index - 1 {
+        quickSort(arr, left, index - 1)
+    }
+    
+    if index < right {
+        quickSort(arr, index, right)
+    }
+}
