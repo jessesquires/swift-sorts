83bad84bbe24f9cf87ee236897b667020d04087e
Jesse Squires <jesse.d.squires@gmail.com>
2014-08-12 22:00:28 -0700

    update tests

--- SwiftSortsTests/SwiftSortsTests.swift
+++ SwiftSortsTests/SwiftSortsTests.swift
@@ -12,12 +12,12 @@ import XCTest
 
 class SwiftSortsTests: XCTestCase {
     
-    let sortedArray : [Int] = [0, 1, 4, 10, 23, 34, 34, 35, 75, 87, 98, 687, 809, 9324]
+    let sortedArray : [Int] = [0, 1, 4, 10, 23, 34, 34, 35, 66, 75, 87, 98, 687, 809, 9324]
     var unsortedArray : [Int]? = nil
     
     override func setUp() {
         super.setUp()
-        self.unsortedArray = [23, 87, 1, 0, 34, 687, 34, 75, 10, 9324, 809, 98, 35, 4]
+        self.unsortedArray = [23, 87, 1, 0, 34, 687, 34, 75, 10, 9324, 809, 98, 35, 4, 66]
     }
     
     override func tearDown() {
